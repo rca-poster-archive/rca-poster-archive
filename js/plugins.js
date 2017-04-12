230759edb3e71f2e53c2587a22bced1297e245d7
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-04-12 10:55:19 +0100

    Tidies up plugins.js file

--- js/plugins.js
+++ js/plugins.js
@@ -1,33 +1,17 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
     var methods = [
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
         'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
         'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
     ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
 
 // Place any jQuery/helper plugins in here.
-
-
 (function($){
- 
+
     $.fn.shuffle = function() {
- 
+
         var allElems = this.get(),
             getRandom = function(max) {
                 return Math.floor(Math.random() * max);
@@ -38,13 +22,13 @@
                 allElems.splice(random, 1);
                 return randEl;
            });
- 
+
         this.each(function(i){
             $(this).replaceWith($(shuffled[i]));
         });
- 
+
         return $(shuffled);
- 
+
     };
- 
+
 })(jQuery);
