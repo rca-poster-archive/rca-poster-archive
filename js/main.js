559f07eb725ea710c85d3599297987a8461413c0
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-04-12 10:56:05 +0100

    Tidies up main.js file. Corrects classnames, removes hover text functions

--- js/main.js
+++ js/main.js
@@ -1,29 +1,18 @@
-$( document ).ready(function() {
-    $('.column img').shuffle();
+$(document).ready(function() {
 
-$( ".column div" ).each(function() {
-  // $( this ).addClass( "foo" );
-  randomwidth = Math.floor(Math.random() * widths.length);
-  randomfloat = Math.floor(Math.random() * floats.length);
-  randomtext = Math.floor(Math.random() * text.length);
+	var widths = ["four", "five", "six"];
+	var floats = ["right", "left"];
 
-// $(this).css("width", width+"%"); 
-$(this).addClass(widths[randomwidth]);
-$(this).addClass(floats[randomfloat]);
-// $(this).addClass(text[randomtext]);
+    $(".poster").each(function() {
+        // $( this ).addClass( "foo" );
+        randomwidth = Math.floor(Math.random() * widths.length);
+        randomfloat = Math.floor(Math.random() * floats.length);
 
-});
- 
-});
+        // $(this).css("width", width+"%");
+        $(this).addClass(widths[randomwidth]);
+        $(this).addClass(floats[randomfloat]);
+        // $(this).addClass(text[randomtext]);
 
-var widths = ["one", "two", "three", "four", "five", "six"];
-var floats = ["right", "left"];
-var text = ["above", "below"];
+    });
 
-
-
-$('.column div').hover(function() {
- $(this).find('.hiddentext').slideDown();
-}, function() {
- $(this).find('.hiddentext').slideUp();
 });
